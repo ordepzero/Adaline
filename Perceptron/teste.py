0008172dd3a215885fba8d711f1818786fc68df6
Ulises PH <pedro.ul@hotmail.com>
2016-09-06 19:31:22 -0300

    Ultimo commit

--- Perceptron/teste.py
+++ Perceptron/teste.py
@@ -5,5 +5,11 @@ Created on Sun Aug 28 21:09:11 2016
 @author: PeDeNRiQue
 """
 
-my_list = [0] * 3
-print(my_list)
+m = [[0.2, 0.4, 0.5],[0.3, 0.6, 0.7], [0.4, 0.8, 0.3]]
+c = [-1, 0.3, 0.7]
+
+m = np.array(m)
+c = np.array(c)
+r = m * c
+print(r)
+print(np.sum(r,axis=0).tolist())
